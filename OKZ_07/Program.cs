1ecea043825a0064b8f19e1c32bba80210a3ca08
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-04-15 21:05:04 +0300

    Добавьте файлы проекта.

---
+++ OKZ_07/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace OKZ_07
+{
+    class Program
+    {
+        public static string Encrypt(string originalString)
+        {
+            if (String.IsNullOrEmpty(originalString))
+            {
+                throw new ArgumentNullException
+                       ("The string which needs to be encrypted can not be null.");
+            }
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoProvider.CreateEncryptor(bytes, bytes), CryptoStreamMode.Write);
+            StreamWriter writer = new StreamWriter(cryptoStream);
+            writer.Write(originalString);
+            writer.Flush();
+            cryptoStream.FlushFinalBlock();
+            writer.Flush();
+            return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+        }
+
+        static byte[] bytes = ASCIIEncoding.ASCII.GetBytes("ZeroCool");
+
+        public static string Decrypt(string cryptedString)
+        {
+            if (String.IsNullOrEmpty(cryptedString))
+            {
+                throw new ArgumentNullException
+                   ("The string which needs to be decrypted can not be null.");
+            }
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream
+                    (Convert.FromBase64String(cryptedString));
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                cryptoProvider.CreateDecryptor(bytes, bytes), CryptoStreamMode.Read);
+            StreamReader reader = new StreamReader(cryptoStream);
+            return reader.ReadToEnd();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть повідомлення: ");
+            string originalString = Console.ReadLine();
+            string cryptedString = Encrypt(originalString);
+            Console.WriteLine($"Зашифроване  повідомлення: {cryptedString}");
+            Console.WriteLine($"Розшифроване повідомлення: {Decrypt(cryptedString)}");
+            Console.ReadKey();
+        }
+    }
+}
